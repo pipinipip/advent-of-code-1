ae4e506993cc479e6ffd0e626c12249578de9aa2
pipinipip <07593.ypo@gmail.com>
2019-12-12 11:04:28 +0100

    del2

--- src/me/pipinipip/adventofcode/Day1.java
+++ src/me/pipinipip/adventofcode/Day1.java
@@ -10,9 +10,23 @@ public class Day1 {
         for(String ln : input) {
             int n = Integer.parseInt(ln);
 
-            Summa += Math.floor(n/3.-2);;
+            double fuel = Math.floor(n/3.-2);
+            Summa += fuel;
+
+            System.out.println("Start fuel: " + fuel);
+            while(fuel >= 1) {
+                fuel = Math.floor(fuel/3.-2);
+
+                if(fuel < 0) fuel = 0;
+
+                Summa += fuel;
+
+                System.out.println(fuel);
+            }
         }
 
         System.out.println(Summa);
+
+
     }
 }
